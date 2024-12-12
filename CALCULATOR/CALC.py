ec399f6a9be961ff11dc6281009c82c8556935d8
shamruth <shamruth2822006@gmail.com>
2024-12-12 14:01:27 +0530

    feat:ADDED A CALCULATOR FOLDER WHICH CONTAINS SIMPLE PYTHON PROGRAM TO IMPLEMENT BASIC CALCULATOR

---
+++ CALCULATOR/CALC.py
@@ -0,0 +1,28 @@
+class Calculator:
+    def UserValues(self):
+        number_1=float(input("ENTER TWO NUMBERS:"))
+        number_2=float(input("ENTER TWO NUMBERS:"))
+        choice=input("\nADDITION(+)\nSUBRACTION(-)\nMULTIPLICATION(*)\nDIVISION(/)\nMODULO(%)\nENTER YOUR CHOICE:")
+        return(number_1,number_2,choice)
+    def ChoiceEvaluation(self):
+        number_1,number_2,choice=self.UserValues()
+        if(choice=="+"):
+            return(number_1+number_2)
+        elif(choice=="-"):
+            return(number_1-number_2)
+        elif(choice=="*"):
+            return(number_1*number_2)
+        elif(choice=="/"):
+            return(number_1/number_2)
+        elif(choice=="%"):
+            return(number_1%number_2)
+loop="Y"
+C=Calculator()
+while(loop!="N"):
+    try:
+
+            result=C.ChoiceEvaluation()
+            print("RESULT OF THE OPERATION IS",result)
+            loop=input("DO YOU WISH TO CONTINUE (Y/N)").upper()
+    except Exception as e:
+        print("AN ERROR OCCUR PLS TRY AGAIN.....")
