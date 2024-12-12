2ce74cf3d3197c3b3b6f3de18e5d15312cbd81ed
shamruth <shamruth2822006@gmail.com>
2024-12-12 17:08:29 +0530

    fix:CORRECTED LOGIC

--- ROCKPAPERSCISSORSGAME.py
+++ ROCKPAPERSCISSORSGAME.py
@@ -1,5 +1,7 @@
 import random
 list=["ROCK","PAPER","SCISSOR"]
+ACCEPTED=[("PAPER","ROCK"),("ROCK","SCISSOR"),("SCISSOR","PAPER")]
+TIE=[("ROCK","ROCK"),("PAPER","PAPER"),("SCISSOR","SCISSOR")]
 loop="Y"
 score=0
 while(loop!="N"):
@@ -8,9 +10,13 @@ while(loop!="N"):
     player_choice=list[int(input("Enter your choice:"))]
     random_choice=random.choice(list)
     print(f"YOUR CHOICE:{player_choice}\nRANDOM CHOICE:{random_choice}")
-    if player_choice != random_choice:
+    temp=(player_choice,random_choice)
+    if (temp in ACCEPTED):
         score+=1
-        continue
+        print("YOU WIN")
+    elif(temp in TIE):
+        score=score
+        print("IT'S A TIE")
     else:
         print("YOU LOST....")
         print(f"TOTAL SCORE:{score}")
