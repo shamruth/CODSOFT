742237c9e4d14b273c40db7105a649207e87f980
shamruth <shamruth2822006@gmail.com>
2024-12-13 12:51:15 +0530

    fix:fixed invaling player input due to usage of player integer input

--- ROCKPAPERSCISSORSGAME.py
+++ ROCKPAPERSCISSORSGAME.py
@@ -1,23 +1,29 @@
 import random
 list=["ROCK","PAPER","SCISSOR"]
 ACCEPTED=[("PAPER","ROCK"),("ROCK","SCISSOR"),("SCISSOR","PAPER")]
-loop="Y"
 score=0
-while(loop!="N"):
+while True:
     for i in range(0,3):
         print(f"{i}.{list[i]}")
     player_choice=int(input("Enter your choice:"))
     random_choice=random.choice(list)
-    print(f"YOUR CHOICE:{player_choice}\nRANDOM CHOICE:{random_choice}")
-    temp=(player_choice,random_choice)
+    print("===========================================================")
+    print(f"YOUR CHOICE:{list[player_choice]}\nRANDOM CHOICE:{random_choice}")
+    print("===========================================================")
+    temp=(list[player_choice],random_choice)
     if (temp in ACCEPTED):
         score+=1
         print("YOU WIN")
-    elif(player_choice==random_choice):
+        print("===========================================================")
+    elif(list[player_choice] == random_choice):
         score=score
         print("IT'S A TIE")
+        print("===========================================================")
     else:
         print("YOU LOST....")
         print(f"TOTAL SCORE:{score}")
+        print("===========================================================")
         score=0
         loop=input("DO YOU WISH TO CONTINUE (Y/N)").upper()
+        if(loop=="N"):
+            break
