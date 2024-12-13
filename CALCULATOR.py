22ae27bfbb666f593ffa16bcb6cc3cd93732be30
shamruth <shamruth2822006@gmail.com>
2024-12-13 11:59:46 +0530

    fix:updated print statement

--- CALCULATOR.py
+++ CALCULATOR.py
@@ -2,8 +2,8 @@ class Calculator:
     def __init__(self):
         print("WELCOME TO THE CALCULATOR PROGRAM")
     def UserValues(self):
-        number_1=float(input("ENTER TWO NUMBERS:"))
-        number_2=float(input("ENTER TWO NUMBERS:"))
+        number_1=float(input("ENTER A NUMBERS:"))
+        number_2=float(input("ENTER A NUMBERS:"))
         choice=input("\nADDITION(+)\nSUBRACTION(-)\nMULTIPLICATION(*)\nDIVISION(/)\nMODULO(%)\nENTER YOUR CHOICE:")
         return(number_1,number_2,choice)
     def ChoiceEvaluation(self):
@@ -21,9 +21,10 @@ class Calculator:
 C=Calculator()
 while True:
     try:
-
             result=C.ChoiceEvaluation()
+            print("========================================")
             print("RESULT OF THE OPERATION IS",result)
+            print("========================================")
     except Exception as e:
         print(e)
         print("AN ERROR OCCUR PLS TRY AGAIN.....")
